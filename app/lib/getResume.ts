9fdce6a332349a618061d55ae7913d3a378d7fa8
scott susanto <susantoscott@gmail.com>
2024-06-24 23:28:46 -0700

    move resume to /public

--- app/lib/getResume.ts
+++ app/lib/getResume.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export function getResume() {
-  const resumePath = path.join(process.cwd(), 'app/lib/data/Resume.md');
+  const resumePath = path.join(process.cwd(), 'public', 'data/Resume.md');
   const resume = fs.readFileSync(resumePath, 'utf-8');
 
   return resume;
